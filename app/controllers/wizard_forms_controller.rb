4c75dd716e6406e5b814a74ea92c75dd10039b58
ikhwanh <ikhwanh.dev@gmail.com>
2021-08-13 08:45:07 +0700

    Setup cookie store as session

--- app/controllers/wizard_forms_controller.rb
+++ app/controllers/wizard_forms_controller.rb
@@ -22,14 +22,14 @@ class WizardFormsController < ApplicationController
   end
 
   def flush
-    @wizard.storage.remove_all
+    @wizard.flush
     redirect_to action: :index
   end
 
   private
 
   def wizard_builder
-    wizard = Wizard.new(storage: CookiesStorage.new(cookies))
+    wizard = Wizard.new(storage: Wizard::SessionStorage.new(session))
     wizard.add_step('Identity') do |step|
       step.add_field('name', label: 'Name', type: 'text', required: true)
       step.add_field('age', label: 'Age', type: 'number', required: true)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.eager_load_paths << Rails.root.join('lib')
+  LibCollection::Application.config.session_store :cookie_store, key: 'development_key', expire_after: 1.day
 end
