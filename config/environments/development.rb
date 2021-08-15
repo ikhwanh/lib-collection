8630cbb99f4497e962c060b41880a6fa780373e1
ikhwanh <ikhwanh.dev@gmail.com>
2021-08-15 17:07:21 +0700

    Setup smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   config.eager_load_paths << Rails.root.join('lib')
   LibCollection::Application.config.session_store :cookie_store, key: 'development_key', expire_after: 1.day
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
